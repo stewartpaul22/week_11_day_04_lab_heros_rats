4913eba7534404bf2d9dee22788c58c3e63ce766
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-04-05 15:44:43 +0100

    Add eat() to Hero with test

--- hero.js
+++ hero.js
@@ -15,4 +15,16 @@ Hero.prototype.addTask = function (task) {
   this.tasks.push(task);
 };
 
+// A hero should be able to eat food, and health should go up by the replenishment value
+Hero.prototype.eat = function (food) {
+  this.health += food.replenishmentVal;
+};
+
+// If the food is their favourite food, their health should go up by 1.5 * value.
+
+// A hero should be able to sort their tasks by difficulty, urgency or reward.
+
+// A hero should be able to view tasks that are marked as completed or incomplete.
+
+
 module.exports = Hero;
--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -1,17 +1,20 @@
 var assert = require('assert');
 var Hero = require('../hero.js');
 var Task = require('../task.js');
+var Food = require('../food.js');
 
 describe('Hero', function(){
 
   var hero1;
   var task1;
   var task2;
+  var food1;
 
   beforeEach(function(){
     hero1 = new Hero("Jeff", 100, "Burgers");
     task1 = new Task(6, 2, 'Sword', false);
     task2 = new Task(5, 5, 'Gold coins', false);
+    food1 = new Food('Bread', 3);
   })
 
   it('should have a name', function(){
@@ -35,5 +38,18 @@ describe('Hero', function(){
     hero1.addTask(task2);
     assert.deepStrictEqual(hero1.tasks, [task1, task2]);
   });
+
+  // A hero should be able to eat food, and health should go up by the replenishment value
+  it('should be able to eat food', function(){
+    hero1.eat(food1);
+    assert.strictEqual(hero1.health, 103);
+  });
+
+  // If the food is their favourite food, their health should go up by 1.5 * value.
+
+  // A hero should be able to sort their tasks by difficulty, urgency or reward.
+
+  // A hero should be able to view tasks that are marked as completed or incomplete.
+
 
 });
